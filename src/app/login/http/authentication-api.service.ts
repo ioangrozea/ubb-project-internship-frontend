139f09c7f93042e3d95ab3fac974602465380a3a
Ioan <ioan.grozea@gmai.com>
2020-05-17 18:44:14 +0300

    - changed login css and added auth service init

--- src/app/login/http/authentication-api.service.ts
+++ src/app/login/http/authentication-api.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {LoginRequest} from "../model/login-request";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {AuthenticationService} from "../service";
 
@@ -15,9 +15,13 @@ export class AuthenticationData {
 @Injectable({providedIn: 'root'})
 export class AuthenticationApiService {
   private authenticationDataSubject: BehaviorSubject<AuthenticationData>;
+  public authenticationData: Observable<AuthenticationData>;
 
   constructor(private http: HttpClient,
               private authenticationService: AuthenticationService) {
+    this.authenticationDataSubject = new BehaviorSubject<AuthenticationData>(
+      JSON.parse(this.authenticationService.getAuthenticationData()));
+    this.authenticationData = this.authenticationDataSubject.asObservable();
   }
 
   get authenticationDataValue(): AuthenticationData {
