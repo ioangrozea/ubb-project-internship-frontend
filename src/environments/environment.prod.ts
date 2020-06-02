f0f674674870955851428797a7ad7f3fefb35141
Ioan <ioan.grozea@gmai.com>
2020-06-02 22:03:21 +0300

    bla

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api_login_url: "https://elixir-account-service.azurewebsites.net",
+  api_profile_url: "https://elixir-profile-service.azurewebsites.net",
+  api_match_url: "https://elixir-match-service.azurewebsites.net",
+  api_chat_url: "https://elixir-chat-service.azurewebsites.net",
 };
