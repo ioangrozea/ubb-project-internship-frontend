f2fbbdc925d97b7c2d0d42c9ed5afe973c72b286
Ioan Grozea <Ioan.Grozea@cloudflight.io>
2020-12-03 18:02:28 +0200

    -positions navbar redirect

--- src/app/shared/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/shared/components/top-nav-bar/top-nav-bar.component.ts
@@ -9,18 +9,22 @@ import {UserType} from '../../../login/model/login-request';
   templateUrl: './top-nav-bar.component.html',
   styleUrls: ['./top-nav-bar.component.css']
 })
-export class TopNavBarComponent extends BaseComponent implements OnInit {
+export class TopNavBarComponent extends BaseComponent {
 
   constructor(private router: Router,
               public authenticationService: AuthenticationService) {
     super();
   }
 
-  ngOnInit() {
-  }
-
   positions() {
-    this.router.navigate(['positions']);
+    switch (+UserType[localStorage.getItem('accountType')]) {
+      case UserType.ROLE_STUDENT:
+        this.router.navigate(['positions/student']);
+        break;
+      case UserType.ROLE_COMPANY:
+        this.router.navigate(['positions/company']);
+        break;
+    }
   }
 
   logOut(){
